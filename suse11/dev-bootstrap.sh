b32ae4dddd4c0ffb4a1c805c54ea7692ab54bc3c
Michael Harp <michael.harp@teradata.com>
2014-08-04 16:46:30 -0700

    Update git version

--- suse11/dev-bootstrap.sh
+++ suse11/dev-bootstrap.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
 cp /vagrant/hosts /etc/hosts
-cp /vagrant/resolv.conf /etc/resolv.conf
+cp /vagrant/resolv_TD.conf /etc/resolv.conf
+rm -rf /etc/zypp/repos.d/SUSE*
+#zypper install iptables curl wget
+cp /vagrant/opensuse.repo /etc/zypp/repos.d/
+zypper  --non-interactive remove gcc43 cpp43 
+zypper --non-interactive install --force-resolution  rrdtool
 
 zypper --non-interactive in wget expect git
 zypper --non-interactive in gcc-c++
 zypper --non-interactive in rpmrebuild
 zypper --non-interactive in java-1_6_0-openjdk-devel
+
+zypper --non-interactive in libcurl-devel
+zypper --non-interactive in libopenssl-devel
+zypper --non-interactive in libexpat-devel
+wget http://git-core.googlecode.com/files/git-1.7.12.tar.gz
+tar zxf git-1.7.12.tar.gz
+cd git-1.7.12
+make prefix=/usr install
+cd -
 
 wget http://www.bizdirusa.com/mirrors/apache/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz
 tar zxvf apache-maven-3.0.5-bin.tar.gz
